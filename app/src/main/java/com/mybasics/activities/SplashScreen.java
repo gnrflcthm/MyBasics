8a218e61e0c8f00bff14816757a28e20688ffc4e
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-11-05 09:52:49 +0800

    Added icons and activities for Splash Screen and Main Activity.

---
+++ app/src/main/java/com/mybasics/activities/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.mybasics.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mybasics.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
