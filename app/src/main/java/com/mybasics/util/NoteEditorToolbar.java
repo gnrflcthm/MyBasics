ce4bc2e395a845f77adc5d8bae50871a0e63fe07
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-11-10 09:34:24 +0800

    Added toolbar for note editor.

---
+++ app/src/main/java/com/mybasics/util/NoteEditorToolbar.java
@@ -0,0 +1,39 @@
+package com.mybasics.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import net.dankito.richtexteditor.android.command.BoldCommand;
+import net.dankito.richtexteditor.android.command.DecreaseIndentCommand;
+import net.dankito.richtexteditor.android.command.IncreaseIndentCommand;
+import net.dankito.richtexteditor.android.command.InsertBulletListCommand;
+import net.dankito.richtexteditor.android.command.InsertNumberedListCommand;
+import net.dankito.richtexteditor.android.command.ItalicCommand;
+import net.dankito.richtexteditor.android.command.RedoCommand;
+import net.dankito.richtexteditor.android.command.UnderlineCommand;
+import net.dankito.richtexteditor.android.command.UndoCommand;
+import net.dankito.richtexteditor.android.toolbar.EditorToolbar;
+
+public class NoteEditorToolbar extends EditorToolbar {
+
+    public NoteEditorToolbar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        initToolbars(context);
+    }
+
+    protected void initToolbars(Context context) {
+        addCommand(new BoldCommand());
+        addCommand(new ItalicCommand());
+        addCommand(new UnderlineCommand());
+        addCommand(new DecreaseIndentCommand());
+        addCommand(new IncreaseIndentCommand());
+        addCommand(new InsertBulletListCommand());
+        addCommand(new InsertNumberedListCommand());
+        addCommand(new UndoCommand());
+        addCommand(new RedoCommand());
+    }
+}
