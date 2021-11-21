34f85e77896a272ca5e98202a4ab48a0816e4882
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-11-21 19:46:27 +0800

    Completed Splash Screen.

--- app/src/main/java/com/mybasics/activities/SplashScreen.java
+++ app/src/main/java/com/mybasics/activities/SplashScreen.java
@@ -1,8 +1,12 @@
 package com.mybasics.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceManager;
 
 import com.mybasics.R;
 
@@ -10,7 +14,32 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        loadThemes();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        Handler handler = new Handler(getMainLooper());
+        handler.postDelayed(() -> {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 1000);
+    }
+
+    private void loadThemes() {
+        String theme = PreferenceManager.getDefaultSharedPreferences(this).getString("app_theme", "Light");
+        switch (theme) {
+            case "Dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case "System Default":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case "Light":
+            default:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
