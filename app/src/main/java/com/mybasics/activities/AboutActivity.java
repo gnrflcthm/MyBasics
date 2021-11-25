1c7a581bfd3f051c13ac475f68ab2e0bb582bea0
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2021-11-25 20:11:36 +0800

    Added About activity in preference fragment.

---
+++ app/src/main/java/com/mybasics/activities/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.mybasics.activities;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.mybasics.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ((TextView) findViewById(R.id.tvDescription))
+                .setText(
+                "MyBasics is a project created by three 3rd year Information Technology students at the University of Santo Tomas during the First Semester of School Year 2021-2022 as a requirement for the course, Mobile Programming (ICS 26011)." +
+                        "\n\nThe goal of this application is to provide compiled features that are important to the everyday mobile usage and interaction of mobile users namely the To-Do list, Notes/Memos, and Reminders. To further enhance the efficiency of these basic features, we incorporated the following convenient user interactions:");
+        ((Toolbar) findViewById(R.id.aboutToolbar)).setNavigationOnClickListener(v -> finish());
+    }
+}
